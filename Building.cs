3c3d6b50d09bedf24d65c3ce2e4c3b07ddc1ea93
Braxton Shoop <braxton.shoop@gmail.com>
2020-11-17 12:42:40 -0600

    urban planning build your city completed

--- Building.cs
+++ Building.cs
@@ -33,9 +33,67 @@ namespace Planner
             _owner = name;
         }
 
+        public void Designer (string designerName)
+        {
+           _designer = designerName;
+        }
+
+    
+   
         static void Main(string[] args)
         {
+            Console.WriteLine();
+            Building FiveOneTwoEigth = new Building("512 8th Avenue");
+            FiveOneTwoEigth.Width = 50;
+            FiveOneTwoEigth.Depth = 30;
+            FiveOneTwoEigth.Stories = 20;
+            FiveOneTwoEigth.Designer("Denzel Washington");
+            FiveOneTwoEigth.Construct();
+            FiveOneTwoEigth.Purchase("Robert Oppenheimer");
 
+            Console.WriteLine(FiveOneTwoEigth._address);
+            Console.WriteLine("---------------");
+            Console.WriteLine("Designed by " + FiveOneTwoEigth._designer);
+            Console.WriteLine("Constructed on " + FiveOneTwoEigth._dateConstructed);
+            Console.WriteLine("Owned by " + FiveOneTwoEigth._owner);
+            Console.WriteLine(FiveOneTwoEigth.Volume + " cubic meters of space");
+
+            Console.WriteLine();
+
+            Building FiveOhFiveSouth = new Building("505 South Avenue");
+            FiveOhFiveSouth.Width = 90;
+            FiveOhFiveSouth.Depth = 70;
+            FiveOhFiveSouth.Stories = 40;
+            FiveOhFiveSouth.Designer("Annette Bening");
+            FiveOhFiveSouth.Construct();
+            FiveOhFiveSouth.Purchase("Robert Frost");
+            
+            Console.WriteLine(FiveOhFiveSouth._address);
+            Console.WriteLine("---------------");
+            Console.WriteLine("Designed by " + FiveOhFiveSouth._designer);
+            Console.WriteLine("Constructed on " + FiveOhFiveSouth._dateConstructed);
+            Console.WriteLine("Owned by " + FiveOhFiveSouth._owner);
+            Console.WriteLine(FiveOhFiveSouth.Volume + " cubic meters of space");
+
+            Console.WriteLine();
+
+            Building TwentyNineWest = new Building("29 West Street");
+            TwentyNineWest.Width = 30;
+            TwentyNineWest.Depth = 20;
+            TwentyNineWest.Stories = 7;
+            TwentyNineWest.Designer("Timothy Olyphant");
+            TwentyNineWest.Construct();
+            TwentyNineWest.Purchase("Robert McNamara");
+            
+            Console.WriteLine(TwentyNineWest._address);
+            Console.WriteLine("---------------");
+            Console.WriteLine("Designed by " + TwentyNineWest._designer);
+            Console.WriteLine("Constructed on " + TwentyNineWest._dateConstructed);
+            Console.WriteLine("Owned by " + TwentyNineWest._owner);
+            Console.WriteLine(TwentyNineWest.Volume + " cubic meters of space");
+
+            Console.WriteLine();
         }
+     
     } 
 }
